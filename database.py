f783b20af3ae3fd48a141d80ddb69e02fbcb5e1b
yusufjonc07 <ahmedovyusufjon3@gmail.com>
2023-08-25 11:50:34 +0500

    wwww

--- database.py
+++ database.py
@@ -6,15 +6,7 @@ from sqlalchemy.orm import sessionmaker
 
 
 
-import os
-BASE_DIR = os.path.dirname(os.path.realpath(__file__))
-SQLALCHEMY_DATABASE_URL = 'sqlite:///'+os.path.join(BASE_DIR,'baza.db?check_same_thread=False')
-SECRET_KEY = 'SOME-SECRET-KEY'
-ALGORITHM = 'HS256'
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True, pool_recycle=300)
-# engine=create_engine('mysql+pymysql://root@localhost:3306/diagnosis')
+engine=create_engine('mysql+pymysql://root@localhost:3306/diagnosis')
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
