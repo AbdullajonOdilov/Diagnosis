b3d7223360a10a977299c9ff9e65d71585554a10
Timurjumaev <Tjumaev2002@gmail.com>
2023-08-29 12:55:09 +0500

    uploaded file router and function fixed

--- routes/uploaded_files.py
+++ routes/uploaded_files.py
@@ -1,9 +1,7 @@
 import inspect
 from typing import List
-
 from fastapi import APIRouter, Depends, UploadFile, Form, File, HTTPException
 from sqlalchemy.orm import Session
-
 from database import database
 from functions.uploaded_files import one_file, all_uploaded_files, create_file, update_file
 from routes.login import get_current_active_user
@@ -20,22 +18,12 @@ uploaded_files_router = APIRouter(
 def get_files(search: str = None, id: int = 0, source: str = None, page: int = 1, limit: int = 25,
               db: Session = Depends(database),
               current_user: CreateUser = Depends(get_current_active_user),
-             ):
+              ):
 
     role_verification(current_user, inspect.currentframe().f_code.co_name)
     if id > 0:
         return one_file(id, db)
     return all_uploaded_files(search, source, page, limit, db)
-
-
-# @uploaded_files_router.post("/create")
-# def file_create(new_files: list[UploadFile], sources: list[str],
-#                 source_ids: list[str], comments: list[str],
-#                 db: Session = Depends(database),
-#                 current_user: CreateUser = Depends(get_current_active_user)):
-#     role_verification(current_user, inspect.currentframe().f_code.co_name)
-#     return create_file(new_files, sources, source_ids, comments, current_user, db)
-#     # raise HTTPException(status_code=200, detail="Amaliyot muvaffaqiyatli amalga oshirildi")
 
 
 @uploaded_files_router.post("/create")
@@ -49,28 +37,29 @@ async def upload_files_with_db(
 ):
     role_verification(current_user, inspect.currentframe().f_code.co_name)
     create_file(new_files, source, source_id, comment, current_user, db)
-    return {"message": f"{len(new_files)} files uploaded and saved in the database"}
+    raise HTTPException(status_code=200, detail="Amaliyot muvaffaqiyatli amalga oshirildi")
 
 
 @uploaded_files_router.put("/update")
 def file_update(
-        id: int = Form(0),
-        new_file: UploadFile = File(None), source: str = Form(None),
-        source_id: int = Form(0), comment: str = Form(None),
+        new_files: List[UploadFile] = File(...),
+        source: str = Form(...),
+        source_id: int = Form(...),
+        comment: str = Form(...),
         db: Session = Depends(database),
         current_user: CreateUser = Depends(get_current_active_user)):
 
     role_verification(current_user, inspect.currentframe().f_code.co_name)
-    update_file(id, new_file, source, source_id, comment, current_user, db)
+    update_file(new_files, source, source_id, comment, current_user, db)
     raise HTTPException(status_code=200, detail="Amaliyot muvaffaqiyatli amalga oshirildi")
 
 
-# @uploaded_files_router.delete("/delete_file")
-# def delete_file(id: int, db: Session = Depends(database),
-#                 current_user: CreateUser = Depends(get_current_active_user)):
-#     role_verification(user=current_user)
-#     delete_file_e(id, db)
-#     raise HTTPException(status_code=200, detail="Amaliyot muvaffaqiyatli amalga oshirildi")
+@uploaded_files_router.delete("/delete_file")
+def delete_file(id: int, db: Session = Depends(database),
+                current_user: CreateUser = Depends(get_current_active_user)):
+    role_verification(user=current_user)
+    delete_file(id, db)
+    raise HTTPException(status_code=200, detail="Amaliyot muvaffaqiyatli amalga oshirildi")
 
 
 
